3256b9aa0f24ed3c7e67e1d2d6665d362aca4cf3
skyguy-charles <cyphersmae@gmail.com>
2026-02-05 11:14:22 +0300

    Delete Backend directory

--- Backend/app/utils/invoice.py
+++
@@ -1,8 +0,0 @@
-import random
-import time
-
-def generate_invoice_number():
-    """Generates a unique invoice number."""
-    timestamp = int(time.time() * 1000)
-    random_part = random.randint(1000, 9999)
-    return f"INV-{timestamp}-{random_part}"

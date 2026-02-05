726acab15aa14510749231717ba2a24bac4465e3
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-05 14:09:33 +0300

    core dependencies continuation of code

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -3,3 +3,8 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 
+from app.database.session import SessionLocal
+from app.database.models import User
+from app.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
